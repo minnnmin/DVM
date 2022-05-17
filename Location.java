0cef722fcbdf53e9c3515827f74d342e81e7387c
3daysJin-MAIN\Lee Seung Jin <3daysjin@gmail.com>
2022-05-17 11:22:25 +0900

    <add> location x, y, getter

--- Location.java
+++ Location.java
@@ -5,16 +5,25 @@ import java.util.*;
  * 
  */
 public class Location {
-
+    private int x;
+    private int y;
     /**
      * Default constructor
      */
-    public Location() {
+    public Location(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
-
     /**
      * 
      */
-    private <int, int> location;
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+
+    //private <int, int> location;
 
 }
