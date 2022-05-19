79c534d6fc0b422d4915b5ea606b44215f87eb82
cj <choijun0627@gmail.com>
2022-05-19 17:12:54 +0900

    controller class checkAdminPassword, setDrinkKinds 작성 완료. setDVMInfo 작성중

--- Controller.java
+++ Controller.java
@@ -148,8 +148,11 @@ public class Controller {
      * @return
      */
     public boolean checkAdminPassword(void String password) {
-        // TODO implement here
-        return false;
+        boolean check=myDVM.isValidPassword();
+        if(!check)
+            return false;
+        else
+            return true;
     }
 
     /**
@@ -160,28 +163,58 @@ public class Controller {
         return null;
     }
 
-    /**
-     * @return
-     */
     public void setDVMInfo() {
-        // TODO implement here
-        return null;
+
+
+        System.out.println("<DVM 정보 관리>");
+        System.out.println("id 입력 후 tab을 눌러 좌표를 입력하세요\n");
+        System.out.println("id  좌표");
+        System.out.println(">");
+
+        while(true)
+        {
+
+        }
     }
 
-    /**
-     * @return
-     */
     public void setDrinkInfo() {
-        // TODO implement here
-        return null;
+
     }
 
-    /**
-     * @return
-     */
     public void setDrinkKinds() {
-        // TODO implement here
-        return null;
+        Scanner scan=new Scanner(System.in);
+        int count=0;
+        int drinkCode=0;
+        int[] dCodeArr=new int[7];
+
+        System.out.println("<음료 세팅>");
+        System.out.println("현재 자판기에서 판매할 7가지 음료의 번호를 차례로 입력해주세요");
+        System.out.println("콜라(01) 사이다(02) 녹차(03) 홍차(04) 밀크티(05) 탄산수(06) 보리차(07) 캔커피(08) 물(09) 에너지드링크(10) " +
+                "바닷물(11) 식혜(12) 아이스티(13) 딸기주스(14) 오렌지주스(15) 포도주스(16) 이온음료(17) 아메리카노(18)" +
+                "핫초로(19) 카페라떼(20)\n");
+        System.out.println(">");
+
+        while(true)
+        {
+            while(!scan.hasNextInt())
+            {
+                scan.next():
+                System.out.println("정확한 번호만 입력하세요");
+            }
+            drinkCode=scan.nextInt();
+            if(drinkCode>=1&&drinkCode<=20)
+            {
+                dCodeArr[count]=drinkCode;
+                count++;
+                if(count==7)
+                    break;
+            }
+            else
+            {
+                System.out.println("정확한 번호만 입력하세요");
+            }
+        }
+        myDVM.saveDrinkKinds(dCodeArr);
     }
 
     /**
