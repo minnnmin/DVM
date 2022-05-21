0953cc37529db4bf014f7a0555aabf11f5285a99
3daysJin-MAIN\Lee Seung Jin <3daysjin@gmail.com>
2022-05-22 05:41:49 +0900

    <add> DVM 구현

--- DVM.java
+++ DVM.java
@@ -1,5 +1,7 @@
 
 import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * 
@@ -10,28 +12,18 @@ public class DVM {
      * Default constructor
      */
     public DVM() {
-        this.location = location(0,0);
+        this.location = Location(0,0);
+        itemList = new Item[20];
+        for(int i = 0; i < 20 ; ++i){
+            itemList[i] = new Item(i);
+        }
+        id = "9999";
     }
-
-    /**
-     * 
-     */
     private String id;
-
-    /**
-     * 
-     */
     private Location location;
-
-    /**
-     * 
-     */
     private Item[] itemList;
+    private Queue<String> vCodeList = new LinkedList<String>();
 
-    /**
-     * 
-     */
-    private String[] vCode;
 
     /**
      * @return
@@ -44,40 +36,41 @@ public class DVM {
     /**
      * @return
      */
-    public boolean checkStock() {
+    public boolean checkStock(int dCode, int Count) {
         // TODO implement here
+        return itemlist[dCode-1].checkStock(Count);
+    }
+
+    /**
+     * @return
+     */
+    public boolean updateStock(int dCode, int Count) {
+        // TODO implement here
+        return itemlist[dCode-1].updateStock(Count);
+    }
+
+    /**
+     * @return
+     */
+
+    public boolean saveVerificationCode(String vCode) {
+        // TODO implement here
+        vCodeList.add(vCode);
         return false;
     }
 
     /**
      * @return
      */
-    public boolean updateStock() {
+    public boolean isValidVerificationCode(String vCode) {
         // TODO implement here
-        return false;
+        return vCodeList.contain(vCode);
     }
 
     /**
      * @return
      */
-
-    public boolean saveVerificationCode(void String vCode) {
-        // TODO implement here
-        return false;
-    }
-
-    /**
-     * @return
-     */
-    public boolean isValidVerificationCode() {
-        // TODO implement here
-        return false;
-    }
-
-    /**
-     * @return
-     */
-    public boolean reqVerificationCodeItem() {
+    public boolean reqVerificationCodeItem(String vCode) {
         // TODO implement here
         return false;
     }
@@ -87,6 +80,7 @@ public class DVM {
      */
     public boolean isValidPassword() {
         // TODO implement here
+        //Password 비교 대상을 어디에 둘지
         return false;
     }
 
@@ -97,6 +91,9 @@ public class DVM {
      */
     public void saveDVMInfo(void String id, void Location location) {
         // TODO implement here
+        this.id = id;
+        this.location.setX(location.getX());
+        this.location.setY(location.getY());
         return null;
     }
 
@@ -109,6 +106,8 @@ public class DVM {
      */
     public void saveDrinkInfo(void int dCode, void int price, void int stock, void String name) {
         // TODO implement here
+        //item setter 필요
+        //itemList[dCode-1];
         return null;
     }
 
@@ -118,6 +117,7 @@ public class DVM {
      */
     public void saveDrinkKinds(void int[] dCode) {
         // TODO implement here
+        // 받아오는 형식과 저장하는 방식의 문제
         return null;
     }
 
